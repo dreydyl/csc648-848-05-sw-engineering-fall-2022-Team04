7ac881284fd7de11e82d51fb05479200649e1c50
youssefh22 <youyoushammoud@gmail.com>
2022-10-19 15:39:59 -0700

    styling complete

--- Milestones/M2/VerticalPrototype/route/listingRoutes.js
+++ Milestones/M2/VerticalPrototype/route/listingRoutes.js
@@ -3,19 +3,28 @@ const listingControllers = require('../controllers/listingControllers');
 const multer = require('multer');
 const router = express.Router();
 const path = require('path');
+const crypto = require('crypto');
 
 const storage = multer.diskStorage({
-    destination: './public/images',
-    filename: (req, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    destination: function(req, file, cb) {
+        cb(null, './public/images');
+    },
+    filename: function(req, file, cb) {
+        let fileExt = file.mimetype.split('/')[1];
+        let randomName = crypto.randomBytes(22).toString("hex");
+        cb(null, `${randomName}.${fileExt}`);
+        //return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
     }
-})
+});
 
 const upload = multer({
     storage: storage
-})
+});
 
-router.route("/").get(listingControllers.getAllUsers).post(upload.single('picture'),listingControllers.createNewUser);
+router.route("/").get(listingControllers.getAllUsers).post(upload.single('picture'),listingControllers.createNewUser, (req, res, next) => {
+    console.log(req);
+    res.send('');
+});
 router.route("/:zipcode").get(listingControllers.getListByZipcode);
 //router.route("/:city").get(listingControllers.getListByCity);
 
