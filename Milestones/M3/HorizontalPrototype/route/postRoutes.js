1190322d9ee2e61af0e4b43c05be3cc74931f262
Devon Dy-Liacco <dreydee33@gmail.com>
2022-10-26 18:29:33 -0700

    Remove unnecessary files

--- Milestones/M3/HorizontalPrototype/route/postRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const postControllers = require('../controllers/postControllers');
-const router = express.Router();
-
-router.route("/").get(postControllers.getAllPosts).post(postControllers.createNewPost);
-
-router.route("/:title").get(postControllers.getPostByTitle);
-
-module.exports = router;
--- Milestones/M3/HorizontalPrototype/route/registeredRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const registerControllers = require('../controllers/registerControllers');
-const router = express.Router();
-
-router.route("/").get(registerControllers.getAllUsers).post(registerControllers.createNewUser);
-
-
-module.exports = router;
