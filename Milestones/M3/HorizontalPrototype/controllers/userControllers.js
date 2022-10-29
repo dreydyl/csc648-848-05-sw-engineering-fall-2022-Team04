4055bf65f24910dcfffe2215c36812bab394d38e
Issashihadeh <issashihadeh20@gmail.com>
2022-10-29 15:52:55 -0700

    completed server-side validation

--- Milestones/M3/HorizontalPrototype/controllers/userControllers.js
+++ Milestones/M3/HorizontalPrototype/controllers/userControllers.js
@@ -10,12 +10,53 @@ exports.getAllUsers =  async  (req, res, next ) => {
         next(error);  
     }
 }
+const checkUsername = (username) => {
+
+    let usernameChecker = /^\D\w{2,}$/;
+    return usernameChecker.test(username);
+    }
+    
+    const checkPassword = (password) => {
+        let passwordChecker = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        return passwordChecker.test(password);
+    }
+    
+    const checkEmail = (email) => {
+        let emailChecker = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return emailChecker.test(email);
+    }
+    
 
 exports.register = async (req, res, next) => {
     try {
         //parse json
         let {name, email, password} = req.body;
         //TODO server-side validation here
+        if(!checkUsername(username)){
+            req.flash('error',"invalid username!!!");
+            req.session.save(err => {
+                res.redirect("/registration");
+        });
+        }else if(!checkEmail(email)){
+            req.flash('error',"invalid Email!!!");
+            req.session.save(err => {
+                res.redirect("/registration");
+        });
+            
+        }else if(!checkPassword(password)){
+            req.flash('error', "Password must be at least8 characters long, contains Upper and lower case characters, and a special character");
+            req.session.save(err => {
+                res.redirect("/registration");
+            })
+    
+        }else if (password != cpassword){
+            req.flash('error', "password did not match");
+            req.session.save(err => {
+                res.redirect("/registration");
+        });
+        }else{
+            next();
+        }
 
         register = await User.register(name, email, password);
 
