ec163d3a48a5c2483dfd2473ab0140b504bbbdc3
Devon Dy-Liacco <dreydee33@gmail.com>
2022-11-01 01:20:59 -0700

    Provide base for frontend prototypes

--- Milestones/M3/HorizontalPrototype/route/routeIndex.js
+++ Milestones/M3/HorizontalPrototype/route/routeIndex.js
@@ -6,4 +6,41 @@ router.get("/", userControllers.getFeaturedLandlords, (req, res, next) => {
     res.render("main",{title:"EZRent Home"});
 });
 
+/* Frontend Tests */
+//to test page, use localhost:8080/route
+//e.g. localhost:8080/postlisting to go to postListingPage
+//will be renamed and migrated to userRoutes.js and listingRoutes.js
+
+router.get("/postlisting", (req, res, next) => {
+    res.render("postListingPage",{title:"EZRent New Listing"});
+});
+
+router.get("/listingpage", (req, res, next) => {
+    res.render("postListingPage",{title:"EZRent Listing"});
+});
+
+router.get("/login", (req, res, next) => {
+    res.render("loginpage",{title:"EZRent Login"});
+});
+
+router.get("/register", (req, res, next) => {
+    res.render("registration",{title:"EZRent New Account"});
+});
+
+router.get("/renter", (req, res, next) => {
+    res.render("profilePage",{title:"EZRent New Account"});
+});
+
+router.get("/landlord", (req, res, next) => {
+    res.render("landlordPage",{title:"EZRent New Account"});
+});
+
+router.get("/help", (req, res, next) => {
+    res.render("helppage",{title:"EZRent New Account"});
+});
+
+router.get("/searchresults", (req, res, next) => {
+    res.render("searchpage",{title:"EZRent New Account"});
+});
+
 module.exports = router;
