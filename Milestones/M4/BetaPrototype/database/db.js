8f6f92e121e0678787ece640280bbff00159cd13
Tung Nguyen <thaotung@sfs-wifi-dhcp-10-143-240-186.wifi.sfsu.edu>
2022-11-17 17:48:36 -0800

    Changed db to webserver for testing

--- Milestones/M4/BetaPrototype/database/db.js
+++ Milestones/M4/BetaPrototype/database/db.js
@@ -1,41 +1,11 @@
-// function createPool() {
-//   try {
-//     const mysql = require('mysql2');
-
-//     const pool = mysql.createPool({
-//       host: 'database-csc648-team4.cwpgbxyiuu7u.us-west-1.rds.amazonaws.com',
-//       port: 3306,
-//       user: 'team4_db_admin',
-//       password: 'Csc648-team4',
-//       database: 'mydb',
-//       connectionLimit: 10,
-//       waitForConnections: true,
-//       queueLimit: 0
-//     });
-//     const promisePool = pool.promise();
-    
-//     return promisePool;
-//   } catch (error) {
-//     return console.log(`Could not connect - ${error}`);
-//   }
-// }
-
-// const pool = createPool();
-
-// module.exports = {
-//   connection: async () => pool.getConnection(),
-//   execute: (...params) => pool.execute(...params)
-// };
-
-
 function createPool() {
   try {
     const mysql = require('mysql2');
 
     const pool = mysql.createPool({
-      host: 'localhost',
+      host: 'database-csc648-team4.cwpgbxyiuu7u.us-west-1.rds.amazonaws.com',
       port: 3306,
-      user: 'root',
+      user: 'team4_db_admin',
       password: 'Csc648-team4',
       database: 'mydb',
       connectionLimit: 10,
@@ -56,3 +26,33 @@ module.exports = {
   connection: async () => pool.getConnection(),
   execute: (...params) => pool.execute(...params)
 };
+
+
+// function createPool() {
+//   try {
+//     const mysql = require('mysql2');
+
+//     const pool = mysql.createPool({
+//       host: 'localhost',
+//       port: 3306,
+//       user: 'root',
+//       password: 'Csc648-team4',
+//       database: 'mydb',
+//       connectionLimit: 10,
+//       waitForConnections: true,
+//       queueLimit: 0
+//     });
+//     const promisePool = pool.promise();
+    
+//     return promisePool;
+//   } catch (error) {
+//     return console.log(`Could not connect - ${error}`);
+//   }
+// }
+
+// const pool = createPool();
+
+// module.exports = {
+//   connection: async () => pool.getConnection(),
+//   execute: (...params) => pool.execute(...params)
+// };
