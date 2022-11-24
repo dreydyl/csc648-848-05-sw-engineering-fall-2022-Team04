1c44cecceca714c7a77c28542bfb077b8509bc2c
Devon Dy-Liacco <dreydee33@gmail.com>
2022-11-23 23:16:55 -0800

    Move script to profilePage.js

---
+++ Milestones/M4/BetaPrototype/public/js/profilePage.js
@@ -0,0 +1,11 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("textarea").value = "";
+    document.getElementById("myForm").style.display = "none";
+}
+document.getElementById("profile-page-listings").onclick = () => {
+    location.replace("/listingpage");
+}
