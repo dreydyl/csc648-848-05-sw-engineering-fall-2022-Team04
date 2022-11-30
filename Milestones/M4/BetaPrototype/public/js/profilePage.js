bf6a67413390046dd6cc3c1df7bb343d517e2fbf
youssefh22 <youyoushammoud@gmail.com>
2022-11-29 17:05:30 -0800

    Frontend homepage and profile pages

--- Milestones/M4/BetaPrototype/public/js/profilePage.js
+++ Milestones/M4/BetaPrototype/public/js/profilePage.js
@@ -1,11 +1,21 @@
-function openForm() {
+document.getElementById("post-review-button").onclick = () => {
     document.getElementById("myForm").style.display = "block";
 }
 
-function closeForm() {
+document.getElementById("post-review-submit-button").onclick = () => {
     document.getElementById("textarea").value = "";
     document.getElementById("myForm").style.display = "none";
 }
+
 document.getElementById("profile-page-listings").onclick = () => {
     location.replace("/listingpage");
 }
+
+// function openForm() {
+//     document.getElementById("myForm").style.display = "block";
+// }
+
+// function closeForm() {
+//     document.getElementById("textarea").value = "";
+//     document.getElementById("myForm").style.display = "none";
+// }
